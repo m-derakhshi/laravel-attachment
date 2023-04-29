089fc719217ac0b15f7c35a70e90d446c69f9520
Masoud Derakhshi <mderakhshi1992@gmail.com>
2023-04-29 09:55:09 +0330

    add HasDatetimeUTCTrait

---
+++ src/Traits/HasDatetimeUTCTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MDerakhshi\LaravelAttachment\Traits;
+
+use MDerakhshi\LaravelAttachment\Eloquent\Casts\DatetimeUTC;
+trait HasDatetimeUTCTrait
+{
+
+    public function getCasts(): array
+    {
+        foreach ($this->casts as $key => $value) {
+            if ($value == 'datetime') {
+                $this->casts[$key] = DatetimeUTC::class;
+            }
+        }
+
+        return parent::getCasts();
+    }
+
+}
